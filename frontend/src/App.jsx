84000c6c52a4057a8ded3814ac1889a687afcb6e
idiotist1 <idiotist1@naver.com>
2025-05-26 11:21:41 +0900

    Ch2-1 메인 배너(Hero.jsx)제작

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import { BrowserRouter } from "react-router-dom";
-import Navbar from './Components/NavBar/Navbar';
-import Footer from './Components/Footer/Footer';
+import React from "react";
+import Navbar from "./Components/NavBar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import MainPage from "./Page/MainPage/MainPage";
+import About from "./Page/About/About";
+import Leadership from "./Page/Leadership/Leadership";
+import Board from "./Page/Board/Board";
+import Services from "./Page/Services/Services";
+import Contact from "./Page/Contact/Contact";
 
-const App = () => {
+function Layout() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
+      <Outlet />
       <Footer />
-    </BrowserRouter>
+    </>
   )
 }
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  }
+])
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
