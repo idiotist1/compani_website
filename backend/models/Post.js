a06ca416c3cff41e38af797828f6d88808c048df
idiotist1 <idiotist1@naver.com>
2025-06-13 16:16:21 +0900

    ch7-3

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,48 +1,50 @@
-const mongoose = require("monggose");
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
-    {
-        number: {
-            type: Number,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        fileUrl: {
-            type: [String],
-            trim: true,
-        },
-        views: {
-            type: Number,
-            default: 0,
-        },
-        viewLogs: [{
-            ip: String,
-            userAgent: String,
-            timestamp: {
-                type: Date,
-                default: Date.now,
-            }
-        }],
-        createAt: {
-            type: Date,
-            default: Date.now,
-        },
-        updateAt: {
-            type: Date,
-            default: Date.now,
-        },
+  {
+    number: {
+      type: Number,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    fileUrl: {
+      type: [String],
+      trim: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    viewLogs: [
+      {
+        ip: String,
+        userAgent: String,
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Post = mongoose.model("Post", postSchema);
